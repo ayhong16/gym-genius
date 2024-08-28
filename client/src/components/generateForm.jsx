45a18a26645460c5caba2fc3382ce90385aac811
Ashley Hong <ayhong8@gmail.com>
2024-08-28 15:20:17 -0400

    update git

--- client/src/components/generateForm.jsx
+++ client/src/components/generateForm.jsx
@@ -1,16 +1,33 @@
 import TimePicker from "./timePicker";
+import { useFormik } from "formik";
 
 
 export default function GenerateForm() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      selectedHours: "",
+      selectedMinutes: "",
+    }
+  })
   return (
-    <div className="flex flex-col">
+    <form>
       <h1 className="text-left text-2xl mb-4">Generate Workout</h1>
-      <h3 className="text-left">Name:</h3>
-      <input type="text" className="border-2 border-black rounded-md p-1 mb-4" />
-      <div className="flex flex-row">
-        <h3 className="text-left">Length:</h3>
-        <TimePicker />
+      <div className="flex flex-col input-container">
+        <h3 className="text-left">Name</h3>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Name your workout"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+          className="border-2 border-black rounded-md p-1 mb-4" />
+        <div className="flex flex-row">
+          <h3 className="text-left">Length:</h3>
+          <TimePicker />
+        </div>
       </div>
-    </div>
+    </form>
   )
 }
